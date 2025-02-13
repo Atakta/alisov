6c14b3a2bdd3f8d27fbc046849aecdc99feed957
Atakta <lemonka_08@mail.ru>
2025-02-13 21:44:29 +0300

    Небольшие правки

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -10,26 +10,26 @@
         <div class="my-10 row flex justify-center">
             @if($catalog)
                 <a
-                    class="rounded-md rounded-r-none bg-black py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-900 focus:shadow-none active:bg-slate-900 hover:bg-slate-900 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                    class="rounded-md rounded-r-none bg-black py-2 px-4 border border-transparent text-center text-sm transition-all shadow-md hover:shadow-lg focus:bg-slate-900 focus:shadow-none active:bg-slate-900 hover:bg-slate-900 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                     href="{{ asset("portfolio") }}"
                 >
                     Все
                 </a>
             @else
                 <span
-                    class="rounded-md rounded-r-none bg-secondary bg-opacity-25 py-2 px-4 border border-transparent text-center text-sm text-white ">
+                    class="rounded-md rounded-r-none bg-secondary bg-opacity-25 py-2 px-4 border border-transparent text-center text-sm">
                     Все
                 </span>
             @endif
             @foreach($cats as $cat)
                 @if($cat->url == $catalog)
                     <span
-                        class="@if($loop->last)rounded-md rounded-l-none @else rounded-none @endif  bg-secondary bg-opacity-25 py-2 px-4 border border-transparent  text-center text-sm text-white transition-all">
+                        class="@if($loop->last)rounded-md rounded-l-none @else rounded-none @endif  bg-secondary bg-opacity-25 py-2 px-4 border border-transparent  text-center text-sm transition-all">
                         {{ $cat->name }}
                     </span>
                 @else
                     <a
-                        class="@if($loop->last)rounded-md rounded-l-none  @else rounded-none @endif bg-black py-2 px-4 border-l border-r border-[#080808] text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-900 focus:shadow-none active:bg-slate-900 hover:bg-slate-900 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                        class="@if($loop->last)rounded-md rounded-l-none  @else rounded-none @endif bg-black py-2 px-4 border-l border-r border-[#080808] text-center text-sm transition-all shadow-md hover:shadow-lg focus:bg-slate-900 focus:shadow-none active:bg-slate-900 hover:bg-slate-900 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                         href="{{ asset("portfolio/" . $cat->url) }}">
                         {{ $cat->name }}
                     </a>
@@ -39,7 +39,7 @@
         <div class="list">
             @foreach($photos as $photo)
                 <div class="pic">
-                    <img src="{{ asset('storage/' . $photo->url) }}" alt="{{ $photo->title }}">
+                    <img class="w-full block" src="{{ asset('storage/' . $photo->url) }}" alt="{{ $photo->title }}">
                 </div>
             @endforeach
         </div>
